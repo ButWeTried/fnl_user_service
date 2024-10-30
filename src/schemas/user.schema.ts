f395a52d93a3d4183721eacac409f19dc2b01f95
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-10-30 04:25:39 +0100

    dev: created a schema for the users

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({
+  timestamps: { createdAt: 'created', updatedAt: 'updated' },
+})
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true, type: 'date' })
+  birthday: string;
+}
+
+const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.index({ email: 1 }, { unique: true });
+export { UserSchema };

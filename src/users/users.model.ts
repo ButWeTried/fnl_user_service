ec1cd034a855674ec6cbf39df76f8f2fb5542b7e
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-10-30 04:34:20 +0100

    dev: creation of the enumerations for the role and setup oof the model that'd be used in the swagger and also as a schema

---
+++ src/users/users.model.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  birthdate: string;
+
+  @Prop()
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

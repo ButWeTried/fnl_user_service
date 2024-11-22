1e2c47bd75c2c6543e430ab4139a0d18672b3825
Cyr Mathieu GUEYE <mgueye@eurocybergroup.fr>
2024-11-22 04:11:34 +0100

    debug: logged the used port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   swaggerConfig(app);
   await app.listen(process.env.APP_PORT);
+  logger.log(`Application listening on port ${process.env.APP_PORT}`);
 }
 
 bootstrap();
